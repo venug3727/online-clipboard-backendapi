fcb2d6f01db6fed5cc2576fb5e33850f83d6a0ab
Venugopal C S <venug3727@gmail.com>
2025-04-16 23:55:48 +0530

    first commit

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
     "http://localhost:5174", 
-    "https://bmsclipboard.netlify.app/"
-        "http://127.0.0.1:5173"],
+    "https://bmsclipboard.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
